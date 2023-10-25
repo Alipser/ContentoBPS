9c411928a3d5177bcb07849b031c9486690f189d
Alipser <romarioajb@outlook.com>
2023-10-25 17:23:19 -0500

    Prueba Tecnica Push server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
-import http from 'http'
-
+import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('¡Hola, mundo!');
+  if (req.url === '/') {
+    // Lee el contenido del archivo HTML
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error interno del servidor');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Página no encontrada');
+  }
 });
 
 const port = 3000;
